353cedd90a1b3d01640582dee35a9a6ae0f06450
Alvaro <aesco96@gmail.com>
2018-03-10 19:09:41 +0100

    funcional

--- model.js
+++ model.js
@@ -1,89 +1,34 @@
 
-const fs = require("fs");
-const preguntas = "quizzes.json";
+const Sequelize = require("sequelize");
 
-let quizzes = [
-    {
-        question: "Tercera letra del abecedario",
-        answer: "C"
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
+
+sequelize.define('quiz', {
+    question: {
+        type: Sequelize.STRING,
+        unique: {msg: "Ya existe esta pregunta"},
+        validate: {notEmpty: {msg: "La respuesta no puede estar vacia"}}
+
     },
-    {
-        question: "Capital de Suecia",
-        answer: "Estocolmo"
-    },
-    {
-        question: "Mejor jugador de baloncesto de la historia",
-        answer: "Michael Jordan"
-    },
-    {
-        question: "Pico más alto del mundo",
-        answer: "Everest"
+    answer: {
+        type: Sequelize.STRING,
+        validate:{notEmpty: {msg: "La respuesta no puede estar vacia"}}
     }
-];
+});
 
-const load = () => {
-    fs.readFile(preguntas, (err, data) => {
-        if(err){
-            if(err.code === "ENOENT"){
-                save();
-                return;
-            }
-            throw err;
-        }
-        let json = JSON.parse(data);
-        if(json){
-            quizzes = json;
-        }
-    });
-};
-
-const save = () => {
-    fs.writeFile(preguntas, JSON.stringify(quizzes), err => {
-        if(err){
-            throw err;
-        }
-    });
-};
-
-exports.count = () => quizzes.length;
-
-exports.add = (question, answer) => {
-    quizzes.push({
-        question: (question || "").trim(),
-        answer: (answer || "").trim()
-    });
-    save();
-};
-
-exports.update = (id, question, answer) => {
-    const quiz = quizzes[id];
-    if (typeof quiz === "undefined"){
-        throw new Error("El valor del parámetro id no es válido");
-    }
-    quizzes.splice(id, 1, {
-        question: (question || "").trim(),
-        answer: (answer || "").trim()
-    });
-    save();
-};
-
-exports.getAll = () => JSON.parse(JSON.stringify(quizzes));
-
-exports.getByIndex = id => {
-    const quiz = quizzes[id];
-    if (typeof  quiz === "undefined"){
-        throw new Error("El valor del parámetro id no es válido");
-    }
-    return JSON.parse(JSON.stringify(quiz));
+sequelize.sync()
+    .then(() => sequelize.models.quiz.count())
+.then(count => {
+    if (!count){
+    return sequelize.models.quiz.bulkCreate([
+        { question: "Capital de Italia", answer:"Roma"},
+        { question: "Capital de Francia", answer:"París"},
+        { question: "Capital de España", answer:"Madrid"},
+        { question: "Capital de Portugal", answer:"Lisboa"}
+    ]);
 }
-
-exports.deleteByIndex = id => {
-    const quiz = quizzes[id];
-    if (typeof  quiz === "undefined"){
-        throw new Error("El valor del parámetro id no es válido");
-    }
-    quizzes.splice(id, 1);
-    save();
-}
-
-load();
+})
+.catch(error => {
+    console.log(error);
+});
+module.exports =sequelize;
